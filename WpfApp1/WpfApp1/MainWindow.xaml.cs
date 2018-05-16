e0505537936867ebd8cc361b2f9e12bdd8bc3b56
LAPTOP-JAD1OL75\User <dream7652210@gmail.com>
2018-05-17 03:44:00 +0800

    完成全部功能和註解

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,51 +20,107 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //紀錄檔案路徑
+        string File_Path = "";
         public MainWindow()
-        {
+        {            
             InitializeComponent();
         }
 
         private void New_Click(object sender, RoutedEventArgs e)
         {
+            //先將Textbox清空
+            TextArea.Text = "";
 
+            //開啟儲存檔案視窗
+            Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+            Nullable<bool> result = dig.ShowDialog();
+
+            if (result == true)
+            {
+                File_Path = dig.FileName;
+                System.IO.File.WriteAllText(File_Path, TextArea.Text);
+            }
+
+            //開啟檔案
+            TextArea.Text = System.IO.File.ReadAllText(dig.FileName);
         }
 
         private void Open_Click_1(object sender, RoutedEventArgs e)
         {
+            //開啟檔案視窗
             Microsoft.Win32.OpenFileDialog dig = new Microsoft.Win32.OpenFileDialog();
             Nullable<bool> result = dig.ShowDialog();
 
             if (result == true)
             {
+                File_Path = dig.FileName;
                 TextArea.Text = System.IO.File.ReadAllText(dig.FileName);
             }
         }
 
         private void Save_Click_2(object sender, RoutedEventArgs e)
         {
+            //假如儲存路徑為空
+            if (File_Path == "")
+            {
+                //則另存新的檔案
+                Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+                Nullable<bool> result = dig.ShowDialog();
+
+                if (result == true)
+                {
+                    File_Path = dig.FileName;
+                    System.IO.File.WriteAllText(File_Path, TextArea.Text);
+                }
+            }
+            else
+            {
+                //不然則直接儲存
+                System.IO.File.WriteAllText(File_Path, TextArea.Text);
+            }
+        }
+
+        //改變字體大小
+        private void A10_Click(object sender, RoutedEventArgs e)
+        {
+            TextArea.FontSize = 10;
+        }
+
+        private void A14_Click(object sender, RoutedEventArgs e)
+        {
+            TextArea.FontSize = 14;
+        }
+
+        private void A18_Click(object sender, RoutedEventArgs e)
+        {
+            TextArea.FontSize = 18;
+        }
+
+        private void Save_as_Click(object sender, RoutedEventArgs e)
+        {
             Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
             Nullable<bool> result = dig.ShowDialog();
 
             if (result == true)
             {
-                string filename = dig.FileName;
+                File_Path = dig.FileName;
+                System.IO.File.WriteAllText(File_Path, TextArea.Text);
             }
         }
 
-        private void A10_Click(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-
+            //將背景顏色改為灰，字體顏色為深灰
+            TextArea.Background = Brushes.DarkGray;
+            TextArea.Foreground = Brushes.Gray;
         }
 
-        private void A14_Click(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void A18_Click(object sender, RoutedEventArgs e)
-        {
-
+            //將背景顏色改為白，字體顏色為深灰
+            TextArea.Background = Brushes.White;
+            TextArea.Foreground = Brushes.DarkGray;
         }
     }
 }
