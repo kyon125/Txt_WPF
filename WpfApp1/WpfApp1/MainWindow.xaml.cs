627402a87d5a257052ae9ad579e8f8218b0cfb9f
LAPTOP-JAD1OL75\User <dream7652210@gmail.com>
2018-05-17 02:20:25 +0800

    開啟 存檔功能完成

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Open_Click_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dig = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = dig.ShowDialog();
+
+            if (result == true)
+            {
+                TextArea.Text = System.IO.File.ReadAllText(dig.FileName);
+            }
+        }
+
+        private void Save_Click_2(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+            Nullable<bool> result = dig.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dig.FileName;
+            }
+        }
+
+        private void A10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A18_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
